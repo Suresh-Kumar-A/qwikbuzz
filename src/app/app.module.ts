18096c15316882059a019354c047232d27aabc57
A.Suresh Kumar <creativegeeksuresh@gmail.com>
2023-11-15 18:45:48 +0530

    additional changes to icon & logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+
+import { CardModule } from "primeng/card";
+import { ButtonModule } from "primeng/button";
+import { ImageModule } from "primeng/image";
+import { CheckboxModule } from "primeng/checkbox";
+import { InputTextModule } from "primeng/inputtext";
+
+import { ToastModule } from "primeng/toast";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DividerModule } from 'primeng/divider';
+import { TagModule } from 'primeng/tag';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { MenuModule } from 'primeng/menu';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+
+import { RippleModule } from 'primeng/ripple';
+import { BadgeModule } from 'primeng/badge';
+import { TooltipModule } from 'primeng/tooltip';
+import { AuthComponent } from './pages/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    CardModule,
+    ButtonModule,
+    ImageModule,
+    CheckboxModule,
+    InputTextModule,
+    ToastModule,
+    DividerModule,
+    TagModule,
+    MenubarModule,
+    AvatarModule,
+    AvatarGroupModule,
+    BreadcrumbModule,
+    MenuModule,
+    RippleModule,
+    BadgeModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
